19a0213e7eac23330e5e1308d32f925be618dbe5
boertel <ben@punchtab.com>
2014-06-09 11:22:56 -0700

    syntax erro

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ exports.social = {
     }
 };
 
-exports.raven: {
+exports.raven = {
     url: "https://d121e324c60f455e86d9ffe47b048304:2883319b17c643df92dd5a0b6ddbce90@app.getsentry.com/118"
 }
 
