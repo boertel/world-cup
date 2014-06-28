37fe3dc8ce08ca0302b865847f41361d9ec3b26d
boertel <ben@punchtab.com>
2014-06-28 11:52:22 -0700

    get ready for d3

---
+++ commands/graph_position.js
@@ -0,0 +1,28 @@
+var db = require('../models');
+
+db.Bet.findAll({
+    where: {
+        validated: true,
+    },
+    include: [
+        {
+            model: db.Game,
+            attributes: db.Game.attrs()
+        },
+        {
+            model: db.User,
+            attributes: db.User.attrs()
+        }
+    ]
+}).success(function (bets) {
+    output = [];
+    bets.forEach(function (bet) {
+        output.push({
+            user_id: bet.user.id,
+            game_id: bet.game.id,
+            points: bet.points(),
+            time: bet.game.time.toISOString()
+        });
+    });
+    console.log(JSON.stringify(output))
+});
---
+++ routes/analytics.js
@@ -0,0 +1,4 @@
+module.exports = {
+    read: function (req, res) {
+    }
+};
