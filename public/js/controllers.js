571f437eac93eeac98ce5580c3375c33bfa01039
boertel <benjamin.oertel@gmail.com>
2016-06-27 15:39:19 -0700

    quarter finals

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,5 @@
 app.controller('HomeController', ['$scope', 'notification', function ($scope, notification) {
-    $scope.groups = '7';
+    $scope.groups = '8';
     $scope.changeGroup = function(groups) {
         $scope.groups = groups;
     };
